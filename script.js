f709e6b50ad8c66b932ce6b55544bcb589d18e08
Ashref Boubakri <otakuachref@gmail.com>
2021-02-25 22:00:30 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+const grad = '1 June 2021';
+
+function countdown(){
+    const gradDate = new Date(grad);
+    const currentDate = new Date();
+    const totalSeconds = (gradDate - currentDate) / 1000;
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600 ) % 24;
+    const minutes = Math.floor(totalSeconds / 60 ) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = formatTime(days);
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(minutes);
+    secondsEl.innerHTML = formatTime(seconds);
+
+    if(Math.floor(totalSeconds) == 0){
+        clearInterval(interval);
+    };
+}
+
+function formatTime(time){
+    return time < 10 ? (`0${time}`) : time;
+}
+
+// Initial call
+countdown();
+
+var interval = setInterval(countdown, 1000);
+
