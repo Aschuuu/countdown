d7ec82626d20086fc678fdc6784c5eadb4210ff4
Aschuuu <otakuachref@gmail.com>
2021-06-29 23:25:15 +0100

    reversed the counter

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const grad = '17 June 2021 09:00:00';
 function countdown() {
     const gradDate = new Date(grad);
     const currentDate = new Date();
-    const totalSeconds = (gradDate - currentDate) / 1000;
+    const totalSeconds = (currentDate - gradDate) / 1000;
     const days = Math.floor(totalSeconds / 3600 / 24);
     const hours = Math.floor(totalSeconds / 3600) % 24;
     const minutes = Math.floor(totalSeconds / 60) % 60;
@@ -19,6 +19,7 @@ function countdown() {
     hoursEl.innerHTML = formatTime(hours);
     minsEl.innerHTML = formatTime(minutes);
     secondsEl.innerHTML = formatTime(seconds);
+    console.log(formatTime(hours))
 
     if (Math.floor(totalSeconds) == 0) {
         clearInterval(interval);
